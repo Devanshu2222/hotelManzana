2f0c804a5e68d949a9a0895c7dfae6e43d6988d0
Devanshu Singh <devanshu9992@gmail.com>
2024-11-18 09:24:14 +0530

    Initial Commit

---
+++ hotelManzana/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hotelManzana
+//
+//  Created by Devanshu Singh(chitkara)     on 18/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ hotelManzanaTests/hotelManzanaTests.swift
@@ -0,0 +1,17 @@
+//
+//  hotelManzanaTests.swift
+//  hotelManzanaTests
+//
+//  Created by Devanshu Singh(chitkara)     on 18/11/24.
+//
+
+import Testing
+@testable import hotelManzana
+
+struct hotelManzanaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
